ee83bbaa7ace3b6ec3a26f52e2f8a36ba84ea45d
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-31 17:12:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {Marley: 'No Woman, No Cry'}
 function updatePlaylist(playlist, artist, songtitle) {
  return Object.assign(playlist, {[artist]: songtitle})
   }
+
+  function removeFromPlaylist(playlist, artist) {
+    return delete playlist.artist
+    }
